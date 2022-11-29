7ccf229abedbec8884585d3fea30dba77d860bb9
epicurithmeticWork <rhsculling@gmail.com>
2022-11-29 14:36:11 +1300

    Commutativity of AND and OR

--- lean/study/tpil4-exercises/tpil4-propositions.lean
+++ lean/study/tpil4-exercises/tpil4-propositions.lean
@@ -1,3 +1,54 @@
+variable (p q r : Prop)
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := 
+  Iff.intro 
+    (λ wpq : p ∧ q =>
+      have wp : p := And.left wpq
+      have wq : q := And.right wpq
+      show q ∧ p from And.intro wq wp)
+    (λ wqp : q ∧ p =>
+      have wq : q := And.left wqp
+      have wp : p := And.right wqp
+      show p ∧ q from And.intro wp wq)
+
+
+example : p ∨ q ↔ q ∨ p := 
+  Iff.intro
+    (λ wpq : p ∨ q =>
+      Or.elim wpq
+        (λ wp : p =>
+          Or.intro_right q wp)
+        (λ wq : q =>
+          Or.intro_left p wq))
+    (λ wqp : q ∨ p =>
+      Or.elim wqp
+        (λ wq : q =>
+          Or.intro_right p wq)
+        (λ wp : p =>
+          Or.intro_left q wp))
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+
 -- Working through exercises from Theorem Proving in Lean 4
 example : (p → (q → r)) →  (p ∧ q → r) := 
   by exact fun t : p → (q → r) =>
